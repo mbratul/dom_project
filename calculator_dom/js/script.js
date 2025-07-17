f038ec7ec23e8003a042faa51b9909bceec1f1c4
Muhtasim Billah Ratul <mbratul@gmail.com>
2025-07-17 16:53:16 +0600

    simple calculator app complete successfully

--- calculator_dom/js/script.js
+++ calculator_dom/js/script.js
@@ -1,1 +1,17 @@
 /* JavaScript Goes Here */
+
+const displayElem = document.querySelector("#display");
+
+function appendToDisplay(input) {
+  displayElem.value += input;
+}
+function clearDisplay() {
+  displayElem.value = "";
+}
+function calculate() {
+  try {
+    displayElem.value = eval(displayElem.value);
+  } catch (error) {
+    displayElem.value = "Error";
+  }
+}
