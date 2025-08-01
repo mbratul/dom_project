2de646a1ef4e17ad5a8c043093cc1ea7ff2d9c32
Muhtasim Billah Ratul <mbratul@gmail.com>
2025-08-01 20:05:25 +0600

    generate hexa decimal color complete

--- generate_hexadecimal_color/js/script.js
+++ generate_hexadecimal_color/js/script.js
@@ -9,3 +9,29 @@
 // Steps
 
 // Step 1 - create onload handler
+window.onload = () => {
+  main();
+};
+
+function main() {
+  const root = document.getElementById("root");
+  const btn = document.getElementById("change-btn");
+  const output = document.getElementById("output");
+
+  btn.addEventListener("click", function () {
+    const bgcolor = generateHEXColor();
+    root.style.backgroundColor = bgcolor;
+    output.value = bgcolor;
+  });
+}
+
+function generateHEXColor() {
+  const red = Math.floor(Math.random() * 255);
+  const green = Math.floor(Math.random() * 255);
+  const blue = Math.floor(Math.random() * 255);
+  const hexadecimal = `#${red.toString(16)}${green.toString(16)}${blue.toString(
+    16
+  )}`;
+
+  return hexadecimal;
+}
