b266f26971f7e99652874f1c5aab385d93b1ec90
Muhtasim Billah Ratul <mbratul@gmail.com>
2025-08-01 19:29:01 +0600

    generate hexadecimal color project start

---
+++ generate_hexadecimal_color/js/script.js
@@ -0,0 +1,11 @@
+/* JavaScript Goes Here */
+"use strict";
+/**
+ * Project Requirements:
+ * - Change the background color by generating random hex color by clicking a button
+ * - Also display the hex code to a disabled input field
+ */
+
+// Steps
+
+// Step 1 - create onload handler
